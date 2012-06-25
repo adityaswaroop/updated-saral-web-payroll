86119c8e46f66041a3aadee0ad05439cbe33dc80
Aditya Swaroop <adi4uenjoy@gmail.com>
2012-06-25 11:37:02 +0530

    Add migration file

--- db/migrate/20120613080105_create_every_month_comp_value.rb
+++ db/migrate/20120613080105_create_every_month_comp_value.rb
@@ -10,7 +10,7 @@ class CreateEveryMonthCompValue < ActiveRecord::Migration
       t.timestamps
     end
     add_index :every_month_comp_values, :employee_id
-    add_index :every_month_comp_values, :paymonth_id
+    #add_index :every_month_comp_values, :paymonth_id
     add_index :every_month_comp_values, :salary_group_id
     add_foreign_key :every_month_comp_values, :employees
     add_foreign_key :every_month_comp_values, :paymonths
