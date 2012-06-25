94acb54a780cf7f273c2246bc48583bea111f843
Aditya Swaroop <adi4uenjoy@gmail.com>
2012-06-25 11:09:49 +0530

    Changing global.rb file

--- lib/assets/global.rb
+++ lib/assets/global.rb
@@ -11,6 +11,6 @@ class Global
   end
 
   def setter_date_format_type
-    #@date_format = DateFormat.find_by_date_format(OptionSetting.first.date_format).date_format_value
+    @date_format = DateFormat.find_by_date_format(OptionSetting.first.date_format).date_format_value
   end
 end
